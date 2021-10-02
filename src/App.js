ed55814f5956adb9941951514cf067238ca41ae7
sandeepraor <sandeepr062003@gmail.com>
2021-10-02 06:09:37 +0530

    i have changed something

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router,Route,Switch } from 'react-router-dom';
 import Navbar from './Components/Navbar/navbar';
 import Sign from './Components/Signin/Sign in';
 import About from './pages/About';
+import Signup from './pages/Signup';
 
 
 function App() {
@@ -10,9 +11,9 @@ function App() {
     <Router>
       <Navbar/>
       <Switch>
-        <Route path='/' component={Sign}/>
+        <Route path='/' component={App}/>
         <Route path='/about' component={About}/>
-        <Route path='/Sign up' component={Sign}/>
+        <Route path='/Sign up' component={Signup}/>
         <Route path='/Sign In' component={Sign}/>
       </Switch>
     </Router>
