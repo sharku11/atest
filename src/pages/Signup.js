11472500faddaf24ed6aac30d69d4d0a53c89c39
sandeepraor <sandeepr062003@gmail.com>
2021-10-02 05:51:38 +0530

    anotherpage

---
+++ src/pages/Signup.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { FlexDiv } from '../Components/Navbar/navbarelements';
+function Signup() {
+    return ( <FlexDiv>
+        <h1>this is Sign up page</h1>
+    </FlexDiv> );
+}
+
+export default Signup;
